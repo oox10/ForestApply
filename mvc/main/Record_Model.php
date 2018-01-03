2fffecfcbcb95b4478bf9b09739366a4eb167cf2
unarus <oos0.0a@gmail.com>
2018-01-03 18:42:12 +0800

    fix apply logs stage counter

--- mvc/main/Record_Model.php
+++ mvc/main/Record_Model.php
@@ -84,7 +84,7 @@
 			$atotal['apply_case']++;
             $atotal['apply_member'] += $dbtmp['member_count'];			
 			
-			if($dbtmp['_stage']==5){
+			if($dbtmp['_stage']>=5){
 			  if($dbtmp['_final']=='核准進入' || $dbtmp['_final']=='申請核准'){
 				
 				$area_apply_record[$apply_index]['accept_case']++;
