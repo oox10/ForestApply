72242a6adf818de1458dc2764cc1ce43c70be15a
unarus <oos0.0a@gmail.com>
2018-03-05 19:23:08 +0800

    update 20180305 last2

--- systemJobs/Job06_InsertOldApply_PD.php
+++ systemJobs/Job06_InsertOldApply_PD.php
@@ -22,7 +22,7 @@
 	$db_update =  $db->DBLink->prepare("INSERT INTO area_booking VALUES(NULL,".
 	":am_id,:apply_code,:apply_date,:applicant_name,:applicant_mail,:applicant_id,:applicant_info,".
 	":apply_reason,:date_enter,:date_exit,:apply_form,:member,:member_count,:check_note,".
-	"0,'0000-00-00',0,0,:_stage,:_progres,:_status,:_final,:_time_create,:_time_update,:_checker,'',1);");
+	"0,'0000-00-00',0,:_review,:_stage,:_progres,:_status,:_final,:_time_create,:_time_update,:_checker,'',1);");
 	
 	$import_stage = 6;
 	
@@ -180,14 +180,13 @@
 		  
 		  $enter_member;
 		  $member_count = count($enter_member);
-		  $check_note = $old_apply['application']['審核備註']; // 審查註記
+		  $check_note   = isset($old_apply['reviewed']['check_node']) ? $old_apply['reviewed']['check_node']:''; // 審查註記
 		  
 		  $_status  = isset($old_apply['application']['狀態']) ? $old_apply['application']['狀態'] : '';
 		  $_final   = isset($old_apply['application']['狀態']) ? $old_apply['application']['狀態'] : '';;
-		  $_stage   = $import_stage;
+		  $_stage   = 0;
 		  
 		  $_progres = ['client'=>[ 0=>[], 1=>[["time"=>date('Y-m-d H:i:s'),"status"=>"資料匯出","note"=>"","logs"=>""]], 2=>[], 3=>[], 4=>[], 5=>[]],'review'=>[ 0=>[], 1=>[], 2=>[], 3=>[], 4=>[], 5=>[]],'admin'=>[ 0=>[], 1=>[], 2=>[], 3=>[], 4=>[], 5=>[]]];
-		  $_progres['client'][5][] = ["time"=>date('Y-m-d H:i:s'),"status"=>$_status,"note"=>"","logs"=>""];
 		  $_progres['admin'][5][] = ["time"=>date('Y-m-d H:i:s'),"status"=>"系統匯入","note"=>"","logs"=>""];
 		  
 		  $_time_create = $apply_date.' 00:00:00';
@@ -195,6 +194,100 @@
 		  $_checker		= '';
 		  
 		  
+		  switch($_status){
+			
+			case '收件待審':
+            
+			    $_stage   = 1;
+			    $areainfo = $areamap[$old_apply['application']['進入區域']];
+			    $apply_bollet  = 0;  //是否需抽籤
+				$applicant_mail='tpcca.rcdh.ntu@gmail.com';
+				
+				// 檢驗申請理由是否需要抽籤
+				
+				if('民眾為環境教育之需要' == $apply_reason){
+				  $apply_bollet = 1;  
+				}
+				// 確認是否抽籤
+				$bollet_date = $apply_bollet ? date('Y-m-d',strtotime('-'.($areainfo['accept_min_day']-1).' day',strtotime($date_enter))) : '0000-00-00';
+				if( $apply_bollet ){  
+				  $_progres['client'][1]   = [array('time'=>date('Y-m-d H:i:s'),'status'=>'收件待審','note'=>'申請項目需抽籤','logs'=>'')];
+				  $_progres['review'][2][] = array('time'=>$bollet_date,'status'=>'系統抽籤','note'=>'','logs'=>'');	
+				}else{
+				   
+				}
+			  break;
+			  
+			case '申請取消':  case '申請註銷': case '申請駁退': case '抽籤未中':
+			  $_progres['client'][5][] = ["time"=>date('Y-m-d H:i:s'),"status"=>$_status,"note"=>"","logs"=>""];
+			  $_stage=5;
+			  $_final=$_status;
+			  break;
+			
+            case '申請核准':
+			  $_progres['client'][5][] = ["time"=>date('Y-m-d H:i:s'),"status"=>'核准進入',"note"=>"","logs"=>""];
+			  $_stage  =5;
+			  $_status = '核准進入';
+			  $_final  ='核准進入';
+			  break;
+			
+			
+			  
+			case '正取送審':
+              $_progres['client'][2][] = array('time'=>date('Y-m-d H:i:s'),'status'=>'正取送審','note'=>date('Y-m-d').'抽籤正取','logs'=>'');
+			  $_stage=3;
+			  $_ballot_result=1;
+			  break;  			
+			  
+			  
+			default: // 其他狀況
+			  
+			  if(preg_match('/^備取(成功|失敗)(.*?)$/',$_status,$match)){
+				
+				$_progres['review'][4][] = array('time'=>date('Y-m-d H:i:s'),'status'=>$match[0],'note'=>$match[2],'logs'=>'');	
+				
+				if($match[1]=='成功'){
+				  $progress['client'][5][] = ['time'=>date('Y-m-d H:i:s'),'status'=>'核准進入','note'=>'','logs'=>''];	
+				}else{
+				  $_progres['client'][5][] = ["time"=>date('Y-m-d H:i:s'),"status"=>'申請註銷',"note"=>"","logs"=>""];	
+				}
+				$_stage = 5;
+				$_final=$_status;
+				
+			  }else if(preg_match('/^備取送審（順序：(\d+)）$/',$_status,$match)){
+				$_status = '備取送審';
+				$_progres['client'][2][] = array('time'=>date('Y-m-d H:i:s'),'status'=>'備取送審','note'=>date('Y-m-d').'抽籤備取 '.$match[1],'logs'=>'');
+			    $_stage=3; 
+				$_ballot_result=2;
+				
+			  }else if(preg_match('/^備取等待（順序：(\d+)）$/',$_status,$match) ){
+				$_status = '備取等待';
+				$_progres['client'][2][] = array('time'=>date('Y-m-d H:i:s'),'status'=>'備取送審','note'=>date('Y-m-d').'抽籤備取 '.$match[1],'logs'=>'');
+			    $_progres['client'][4][] = array('time'=>date('Y-m-d H:i:s'),'status'=>'備取等待','note'=>'','logs'=>'');	
+				$_stage=4;   
+			    $_ballot_result=2;
+			  
+			  }else if(preg_match('/^(.*?)補件/',$_status,$match)){
+                
+				$_progres['client'][2][] = array('time'=>date('Y-m-d H:i:s'),'status'=>$match[1].'送審','note'=>'抽籤'.$match[1],'logs'=>'');
+			    $_progres['client'][3][] = array('time'=>date('Y-m-d H:i:s'),'status'=>'資料不全','note'=>$_status,'logs'=>'');	
+				$_status = '資料不全';
+				$_stage=3;
+			    $_ballot_result=$match[1]=='正取'?1:2;				
+				
+			  }else{
+				/*  處理其他相關狀態*/
+				/*
+				[正取補件送審] => 2
+				
+				*/  
+			  }
+			  break;
+		  
+		  }
+		  
+		  
+		  if(!$_stage) continue;
 		  
 		  $db_update->bindValue(':am_id',$am_id);
 		  $db_update->bindValue(':apply_code',$apply_code);
@@ -211,7 +304,7 @@
 		  $db_update->bindValue(':member',json_encode($enter_member));
 		  $db_update->bindValue(':member_count',$member_count);
 		  $db_update->bindValue(':check_note',$check_note);
-		  
+		  $db_update->bindValue(':_review',isset($old_apply['reviewed']['_review']) ? intval($old_apply['reviewed']['_review']):0 );
 		  $db_update->bindValue(':_stage',$_stage);
 		  $db_update->bindValue(':_progres',json_encode($_progres));
 		  $db_update->bindValue(':_status',$_status);
