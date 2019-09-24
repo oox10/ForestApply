ad49b299c5eba9caa9c0d38a2b955f9c4704361c
unarus <oos0.0a@gmail.com>
2019-09-24 12:59:13 +0800

    UPD:20190924

--- mvc/main/ApplyApi_Controller.php
+++ mvc/main/ApplyApi_Controller.php
@@ -21,6 +21,10 @@ class ApplyApi_Controller extends Admin_Controller{
 	  //exit(1);
 	}
 	
+	// JSON: 申請區域清單 
+	public function index(){   
+	  $this->OpenAPI();
+	}
 	
 	// JSON: 申請區域清單 
 	public function paareas($Group=''){   
@@ -150,6 +154,48 @@ class ApplyApi_Controller extends Admin_Controller{
 	public function OpenAPI(){
 	  $this->Model->OpenAPI_Config_Json();
 	  self::data_output('json-oai','',$this->Model->ModelResult);
+	}
+	
+	
+	
+	
+	// 測試
+	public function TestAPI($API){
+		//登入
+		$applicant = '{"applicant_name":"HSIAO","applicant_userid":"T122806987","applicant_mail":"hsiaoiling@ntu.edu.tw","source":"MTAPI"}';
+		$application = '{"area":{"code":"c6261eb8","inter":["測試進入範圍"],"gate":{"entr":"塔曼山","entr_time":"05:00:00","exit":"小烏來","exit_time":"18:00:00"}},"reason":[{"item":"民眾為環境教育之需要","limit":1}],"attach":[{"code":"C2E1B82C.pdf","time":"2019-09-24 11:24:17","file":"行政院及所屬各機關資訊安全管理要點"}],"dates":[["2019-10-09","2019-10-09"]],"fields":{"application_field_1":{"field":"一、請完整填寫申請當日行進路線【包含預計抵達時間及地點、並說明抵達後從事之行為種類】：","value":"1、08:00由滿月圓進入→10:30水源地→10:40木屋遺址→12:20插天山→13:30木屋遺址→13:40水源地→15:50由滿月圓離開。"},"application_field_2":{"field":"二、環境維護措施(垃圾、廢棄物處理方式)及環境教育內容簡介：","value":"1、全體隊員遵守進入管制規定及無痕山林規範。\n2、不隨意離開已開放供使用之步道及區域。"},"application_field_3":{"field":"三、緊急災難處理(應變相關裝備概述、辦理保險及撤退路線等說明)：","value":"1.裝備：無線電*4隻 (無線電頻率153.300)，地圖、指北針、高度計、手機、山刀、繩索、小急救包、登山\n定位GPS-550T、通訊裝備：手機數支等。\n2.保險:200萬意外,20萬意外醫療,保險期間:2月8日05:00時至2月9日05:00時。\n3.留守人員姓名：OOO、聯絡電話：OOOOOOOOO。\n4.撤退方式：如身體不適或天氣變化或路線受阻，立即原路撤退下山、選擇OOO路線撤退。"}}}';
+		$member = '[{"member_role":"領隊","member_name":"HSIAO","member_id":"T122806987","member_birth":"1980-01-01","member_sex":"男","member_tel":"0999999999","member_cell":"0999999999","member_addr":"台北市","member_org":"單位1","member_contacter":"陳OO","member_contactto":"0911111111"},{"member_role":"成員","member_name":"陳XX","member_id":"A220133129","member_birth":"1981-01-01","member_sex":"女","member_tel":"0922222222","member_cell":"0922222222","member_addr":"台中市","member_org":"單位2","member_contacter":"陳OO","member_contactto":"0911111111"},{"member_role":"成員","member_name":"林ZZ","member_id":"A104698418","member_birth":"1982-01-01","member_sex":"男","member_tel":"0933333333","member_cell":"0933333333","member_addr":"台南市","member_org":"單位3","member_contacter":"陳OO","member_contactto":"0911111111"}]';
+		
+		
+		$url = "https://forestapply.oo10.co/ApplyApi/";
+		
+		$ch 	= curl_init();
+		$options = array(CURLOPT_URL => $url.$API,
+                   CURLOPT_HEADER => 1,
+                   CURLOPT_RETURNTRANSFER => true,
+                   CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2",
+				   CURLOPT_REFERER   => "https://forestapply.oo10.co/",
+				   CURLOPT_COOKIEFILE => 'D:\webroot\O_O\cookie2.txt',
+                   CURLOPT_COOKIEJAR => 'D:\webroot\O_O\cookie2.txt',
+                   CURLOPT_FOLLOWLOCATION => true ,
+				   CURLOPT_SSL_VERIFYPEER => 0,
+                   CURLOPT_SSL_VERIFYHOST => 2,
+                   CURLOPT_CAINFO => getcwd() . "D:\webroot\O_O\CAcerts\GTECyberTrustGlobalRoot.crt",
+				   CURLOPT_POST => 1,
+                   CURLOPT_POSTFIELDS => "applicant=".$applicant
+                  );
+		curl_setopt_array($ch, $options);
+		
+		print_r(curl_exec($ch),true);
+		
+		$output = curl_exec($ch);
+		curl_close($ch);
+		
+		echo "<pre>";
+		print_r(json_decode($output,true),true);
+		
+		 
+		
 	}
 	
 	
